cd5f068f571c04dd22c15a7c78a11d7379231c49
Gabriel Coelho <gabrielcuei98@gmail.com>
2022-10-22 00:17:10 +0100

    added avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,11 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1558102400-72da9fdbecae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img 
+          className={styles.avatar}
+          src="https://github.com/bielcoelho.png"
+         />
+
         <strong>Gabriel Coelho</strong>
         <span>Web developer</span>
       </div>
