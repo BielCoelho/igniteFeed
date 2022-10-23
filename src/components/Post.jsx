66fe76bc7f378772832c1cdcae408663468d9842
Gabriel Coelho <gabrielcuei98@gmail.com>
2022-10-23 19:19:22 +0100

    made avatar a component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img src="https://github.com/bielcoelho.png" />
+          <Avatar src="https://github.com/bielcoelho.png" />
           <div className={styles.authorInfo}>
             <strong>Gabriel Coelho</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -9,7 +10,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1558102400-72da9fdbecae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img 
+        <Avatar 
           className={styles.avatar}
           src="https://github.com/bielcoelho.png"
          />
