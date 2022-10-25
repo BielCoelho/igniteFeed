f04a17fd3ecfdd4cb6a333749fc65eb2c2ca0d0a
Gabriel Coelho <gabrielcuei98@gmail.com>
2022-10-25 18:52:21 +0100

    with ts

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import styles from "./app.module.css";
+import { Header } from "./components/Header";
+import { Post, PostProps } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+import { Splash } from "./components/Splash";
+import "./global.css";
+
+interface Posts extends PostProps {
+  id: number
+}
+
+export function App() {
+  const [loading, setLoading] = useState(true);
+  const [finished, setFinished] = useState(false);
+  const posts: Posts[]  = [
+    {
+      id: 1,
+      author: {
+        avatarUrl: 'https://github.com/bielcoelho.png',
+        name: 'Gabriel Coelho',
+        role: 'Student @Rocketseat'
+      },
+      content: [
+        {type: 'paragraph', content:'Fala galeraa 👋'},
+        {type: 'paragraph', content:' Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat 🚀'},
+        {type: 'paragraph', content:' O projeto é o IgniteFeed '},
+        {type: 'link', content:'jane.design/doctorcare'},
+      ],
+      publishedAt: new Date('2022-10-23 20:00:00'),
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl: 'https://github.com/KevinPagliuca.png',
+        name: 'Kevin Pagliuca',
+        role: 'Experienced Developer'
+      },
+      content: [
+        {type: 'paragraph', content:'Salve Salve.'},
+        {type: 'paragraph', content:' Estou usando da minha experiencia pra ajudar meu webamigo Biel, ele tá indo muito bem 🚀'},
+        {type: 'paragraph', content:' Espero que ele evolua muito, e pode contar comigo'},
+      ],
+      publishedAt: new Date('2022-10-20 20:00:00'),
+    },
+  ]
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+    setTimeout(() => {
+      setFinished(true);
+    }, 2500);
+  }, []);
+
+  return (
+    <div className={loading ? styles.loading : undefined}>
+      <Splash loading={loading} />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (<Post 
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+             />)
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Comment.tsx
@@ -0,0 +1,57 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({content, onDeleteComment}: CommentProps) {
+
+  const [likeCount, setLikeCount] = useState(0)
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/bielcoelho.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gabriel Coelho</strong>
+              <time
+                title="Publicado 23 de outubro de 2022 ás 10:12"
+                dateTime="23-10-2022 10:12:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
